363c46e1dd6dea8b06662413865ea89b7db719a7
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-26 07:36:10 -0700

    Test compatibility

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const spawn = require('cross-spawn');
 const Queue = require('queue-cb');
 
-function exec(cmd, args, callback) {
-  console.log('----------------------');
-  console.log([cmd].concat(args).join(' '));
-  console.log('----------------------');
+function exec(cmd, args, options, callback) {
+  if (!options.silent) {
+    console.log('----------------------');
+    console.log([cmd].concat(args).join(' '));
+    console.log('----------------------');
+  }
+
   var child = spawn(cmd, args, { stdio: 'inherit' });
   child.on('error', callback).on('close', function (exitCode) {
     if (exitCode === 0) return callback();
@@ -15,23 +18,24 @@ function exec(cmd, args, callback) {
 }
 
 module.exports = function nvs(versions, command, options, callback) {
+  if (typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
   options = options || {};
   var queue = new Queue(1);
-  var counter = 0;
 
   var platform = options.platform || process.platform;
   if (platform === 'win32') {
-    console.log('Windows versions not supported yet. Using system version of Node.js');
-    counter++;
-    queue.defer(exec.bind(null, command.shift(), command));
+    options.silent || console.log('Windows versions not supported yet. Using system version of Node.js');
+    queue.defer(exec.bind(null, command.shift(), command, options));
   } else {
-    for (var version of versions) {
-      counter++;
-      queue.defer(exec.bind(null, 'nave', ['use', version].concat(command)));
+    for (var index = 0; index < versions.length; index++) {
+      queue.defer(exec.bind(null, 'nave', ['use', versions[index]].concat(command), options));
     }
   }
 
   queue.await(function (err) {
-    err ? callback(err) : callback(null, counter);
+    err ? callback(err) : callback(null, platform === 'win32' ? 1 : versions.length);
   });
 };
