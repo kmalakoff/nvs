160e243e0f6d2f370a7d58667b99131565bbe05f
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-26 07:36:10 -0700

    Test compatibility

---
+++ test/spec/platform.test.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+
+var nvs = require('../..');
+
+describe('platform', function () {
+  it('native platform', function (done) {
+    nvs(['12', '14'], ['node', '--version'], { silent: true }, function (err, count) {
+      assert.ok(!err);
+      assert.equal(count, process.platform === 'win32' ? 1 : 2);
+      done();
+    });
+  });
+
+  it('win32 platform', function (done) {
+    nvs(['12', '14'], ['node', '--version'], { platform: 'win32', silent: true }, function (err, count) {
+      assert.ok(!err);
+      assert.equal(count, 1);
+      done();
+    });
+  });
+});
