3db7fb5231ef2d28e167d0f8962678d2b1383ef5
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-10 11:23:07 -0700

    Added engine check

--- test/spec/versions.test.js
+++ test/spec/versions.test.js
@@ -1,39 +1,49 @@
 var assert = require('assert');
+var path = require('path');
 
 var nvs = require('../..');
 
 describe('versions', function () {
   describe('happy path', function () {
     it('one version', function (done) {
-      nvs(['14'], 'node', ['--version'], { silent: true }, function (err) {
+      nvs('node', ['--version'], { versions: '14', cache: true, silent: true }, function (err) {
         assert.ok(!err);
         done();
       });
     });
 
     it('latest version', function (done) {
-      nvs(['latest'], 'node', ['--version'], { silent: true }, function (err) {
+      nvs('node', ['--version'], { versions: 'latest', cache: true, silent: true }, function (err) {
         assert.ok(!err);
         done();
       });
     });
 
     it('lts version', function (done) {
-      nvs(['lts'], 'node', ['--version'], { silent: true }, function (err) {
+      nvs('node', ['--version'], { versions: 'lts', cache: true, silent: true }, function (err) {
         assert.ok(!err);
         done();
       });
     });
 
     it('lts/argon version', function (done) {
-      nvs(['lts/argon'], 'node', ['--version'], { silent: true }, function (err) {
+      nvs('node', ['--version'], { versions: 'lts/argon', cache: true, silent: true }, function (err) {
         assert.ok(!err);
         done();
       });
     });
 
     it('multiple versions', function (done) {
-      nvs(['12', '14', 'lts'], 'node', ['--version'], { silent: true }, function (err) {
+      nvs('node', ['--version'], { versions: ['12', '14', 'lts'], cache: true, silent: true }, function (err) {
+        assert.ok(!err);
+        done();
+      });
+    });
+
+    it('using engines', function (done) {
+      nvs('node', ['--version'], { engines: true, cache: true, cwd: path.resolve(path.join(__dirname, '..', 'data', 'engines')), silent: true }, function (
+        err
+      ) {
         assert.ok(!err);
         done();
       });
@@ -42,24 +52,55 @@ describe('versions', function () {
 
   describe('unhappy path', function () {
     it('no versions', function (done) {
-      nvs([], 'node', ['--version'], { silent: true }, function (err) {
+      nvs('node', ['--version'], { cache: true, silent: true }, function (err) {
+        assert.ok(!!err);
+        done();
+      });
+    });
+
+    it('no versions in list', function (done) {
+      nvs('node', ['--version'], { versions: [], cache: true, silent: true }, function (err) {
         assert.ok(!!err);
         done();
       });
     });
 
     it('invalid versions', function (done) {
-      nvs(['1.d.4'], 'node', ['--version'], { silent: true }, function (err) {
+      nvs('node', ['--version'], { versions: ['1.d.4'], cache: true, silent: true }, function (err) {
         assert.ok(!!err);
         done();
       });
     });
 
     it('invalid versions', function (done) {
-      nvs(['14', 'bob'], 'node', ['--version'], { silent: true }, function (err) {
+      nvs('node', ['--version'], { versions: ['14', 'bob'], cache: true, silent: true }, function (err) {
         assert.ok(!!err);
         done();
       });
+    });
+
+    it('engines missing', function (done) {
+      nvs(
+        'node',
+        ['--version'],
+        { engines: true, cache: true, cwd: path.resolve(path.join(__dirname, '..', 'data', 'engines-missing')), silent: true },
+        function (err) {
+          assert.ok(!!err);
+          done();
+        }
+      );
+    });
+
+    it('engines node missing', function (done) {
+      nvs(
+        'node',
+        ['--version'],
+        { engines: true, cache: true, cwd: path.resolve(path.join(__dirname, '..', 'data', 'engines-node-missing')), silent: true },
+        function (err) {
+          assert.ok(!!err);
+          done();
+        }
+      );
     });
   });
 });
