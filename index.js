c5f34387ad8477c52008120df0321a5df969e6e9
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-15 19:49:12 -0700

    Migrated to node-version-use

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+var nvu = require('node-version-use');
 var Queue = require('queue-cb');
 
-var exec = require('./lib/exec');
 var resolveVersions = require('./lib/resolveVersions');
 
 module.exports = function nvs(command, args, options, callback) {
@@ -9,23 +9,30 @@ module.exports = function nvs(command, args, options, callback) {
     options = {};
   }
   options = options || {};
+
   resolveVersions(options, function (err, versions) {
     if (err) return callback(err);
     if (!versions.length) return callback(new Error('No versions provided'));
 
+    var res = typeof options.stdout === 'string' ? { stdout: '', count: 0 } : { count: 0 };
     var queue = new Queue(1);
-    var platform = options.platform || process.platform;
-    if (platform === 'win32') {
-      options.silent || console.log('Windows versions not supported yet. Using system version of Node.js');
-      queue.defer(exec.bind(null, command, args, options));
-    } else {
-      for (var index = 0; index < versions.length; index++) {
-        queue.defer(exec.bind(null, 'nave', ['use', versions[index], command].concat(args), options));
-      }
-    }
+    for (var index = 0; index < versions.length; index++) {
+      var version = versions[index];
+      queue.defer(function (callback) {
+        console.log('----------------------');
+        console.log(version);
+        console.log('----------------------');
 
+        nvu(version, command, args, options, function (err, _res) {
+          if (err || _res.code !== 0) return callback(err || new Error('Failed to use version: ' + version.version + '. Code: ' + _res.code));
+          if (typeof options.stdout === 'string') res.stdout += _res.stdout;
+          res.count++;
+          callback();
+        });
+      });
+    }
     queue.await(function (err) {
-      err ? callback(err) : callback(null, platform === 'win32' ? 1 : versions.length);
+      err ? callback(err) : callback(null, res);
     });
   });
 };
--- test/spec/cli.test.js
+++ test/spec/cli.test.js
@@ -2,54 +2,78 @@ var assert = require('assert');
 var path = require('path');
 var spawn = require('cross-spawn-cb');
 
+var NODE = process.platform === 'win32' ? 'node.exe' : 'node';
+var EOL = /\r\n|\r|\n/;
+
 describe('cli', function () {
   describe('happy path', function () {
     it('one version', function (done) {
-      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', '14', '--cache', 'npm', 'whoami'], { stdio: 'inherit' }, function (err, res) {
+      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', '12', '--cache', 'npm', 'whoami'], { stdout: 'string' }, function (err, res) {
         assert.ok(!err);
-        assert.equal(res.code, 0);
+        assert.ok(res.code === 0);
+        assert.ok(res.stdout.split(EOL).slice(-2, -1)[0].length > 1);
         done();
       });
     });
 
     it('multiple versions', function (done) {
-      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', '12,14', '--cache', 'npm', 'whoami'], { stdio: 'inherit' }, function (err, res) {
+      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', 'lts/argon,12', '--cache', 'npm', 'whoami'], { stdout: 'string' }, function (
+        err,
+        res
+      ) {
         assert.ok(!err);
-        assert.equal(res.code, 0);
+        assert.ok(res.code === 0);
+        assert.ok(res.stdout.split(EOL).slice(-2, -1)[0].length > 0);
         done();
       });
     });
 
     it('one version with options', function (done) {
-      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', '14', '--cache', '--', 'node', '--version'], { stdio: 'inherit' }, function (
+      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', 'lts/erbium', '--cache', '--', NODE, '--version'], { stdout: 'string' }, function (
         err,
         res
       ) {
         assert.ok(!err);
-        assert.equal(res.code, 0);
+        assert.ok(res.code === 0);
+        assert.equal(res.stdout.split(EOL).slice(-2, -1)[0], 'v12.16.3');
+        done();
+      });
+    });
+
+    it('one version with options', function (done) {
+      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', 'lts/argon', '--cache', '--', NODE, '--version'], { stdout: 'string' }, function (
+        err,
+        res
+      ) {
+        assert.ok(!err);
+        assert.ok(res.code === 0);
+        assert.equal(res.stdout.split(EOL).slice(-2, -1)[0], 'v4.9.1');
         done();
       });
     });
 
     it('multiple versions with options', function (done) {
-      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', '12,14', '--cache', '--', 'node', '--version'], { stdio: 'inherit' }, function (
-        err,
-        res
-      ) {
-        assert.ok(!err);
-        assert.equal(res.code, 0);
-        done();
-      });
+      spawn(
+        path.join(__dirname, '..', '..', 'bin', 'nvs'),
+        ['--versions', '10,12,lts/erbium,latest', '--cache', '--', NODE, '--version'],
+        { stdout: 'string' },
+        function (err, res) {
+          assert.ok(!err);
+          assert.ok(res.code === 0);
+          assert.equal(res.stdout.split(EOL).slice(-2, -1)[0], 'v13.14.0');
+          done();
+        }
+      );
     });
 
     it('using engines', function (done) {
       spawn(
         path.join(__dirname, '..', '..', 'bin', 'nvs'),
-        ['--engines', '--cache', '--', 'node', '--version'],
-        { stdio: 'inherit', cwd: path.resolve(path.join(__dirname, '..', 'data', 'engines')) },
+        ['--engines', '--cache', '--', NODE, '--version'],
+        { stdout: 'string', cwd: path.resolve(path.join(__dirname, '..', 'data', 'engines')) },
         function (err, res) {
           assert.ok(!err);
-          assert.equal(res.code, 0);
+          assert.ok(res.code === 0);
           done();
         }
       );
@@ -58,7 +82,7 @@ describe('cli', function () {
 
   describe('unhappy path', function () {
     it('missing command', function (done) {
-      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', '--cache'], { stdio: 'inherit' }, function (err, res) {
+      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', '--cache'], { stdout: 'string' }, function (err, res) {
         assert.ok(!err);
         assert.ok(res.code !== 0);
         done();
@@ -66,7 +90,7 @@ describe('cli', function () {
     });
 
     it('missing versions', function (done) {
-      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', '--cache', '--', 'node', '--version'], { stdio: 'inherit' }, function (err, res) {
+      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', '--cache', '--', NODE, '--version'], { stdout: 'string' }, function (err, res) {
         assert.ok(!err);
         assert.ok(res.code !== 0);
         done();
@@ -74,7 +98,7 @@ describe('cli', function () {
     });
 
     it('invalid versions', function (done) {
-      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', 'junk,junk', '--cache', '--', 'node', '--version'], { stdio: 'inherit' }, function (
+      spawn(path.join(__dirname, '..', '..', 'bin', 'nvs'), ['--versions', 'junk,junk', '--cache', '--', NODE, '--version'], { stdout: 'string' }, function (
         err,
         res
       ) {
@@ -87,8 +111,8 @@ describe('cli', function () {
     it('engines missing', function (done) {
       spawn(
         path.join(__dirname, '..', '..', 'bin', 'nvs'),
-        ['--engines', '--cache', '--', 'node', '--version'],
-        { stdio: 'inherit', cwd: path.resolve(path.join(__dirname, '..', 'data', 'engines-missing')) },
+        ['--engines', '--cache', '--', NODE, '--version'],
+        { stdout: 'string', cwd: path.resolve(path.join(__dirname, '..', 'data', 'engines-missing')) },
         function (err, res) {
           assert.ok(!err);
           assert.ok(res.code !== 0);
@@ -100,8 +124,8 @@ describe('cli', function () {
     it('engines node missing', function (done) {
       spawn(
         path.join(__dirname, '..', '..', 'bin', 'nvs'),
-        ['--engines', '--cache', '--', 'node', '--version'],
-        { stdio: 'inherit', cwd: path.resolve(path.join(__dirname, '..', 'data', 'engines-node-missing')) },
+        ['--engines', '--cache', '--', NODE, '--version'],
+        { stdout: 'string', cwd: path.resolve(path.join(__dirname, '..', 'data', 'engines-node-missing')) },
         function (err, res) {
           assert.ok(!err);
           assert.ok(res.code !== 0);
