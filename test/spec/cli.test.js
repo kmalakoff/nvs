c671bb6d8920fcbc325464de2afff4e1f2c1826a
Kevin Malakoff <kmalakoff@gmail.com>
2020-06-04 19:50:43 -0700

    Use node-version-use

--- test/spec/cli.test.js
+++ test/spec/cli.test.js
@@ -5,123 +5,14 @@ var isVersion = require('is-version');
 var cr = require('cr');
 
 var CLI = path.join(__dirname, '..', '..', 'bin', 'nvs.js');
-var NODE = process.platform === 'win32' ? 'node.exe' : 'node';
 
 describe('cli', function () {
-  describe('happy path', function () {
-    it('one version - 12', function (done) {
-      spawn(CLI, ['12', '--silent', 'npm', '--version'], { stdout: 'string' }, function (err, res) {
-        assert.ok(!err);
-        var lines = cr(res.stdout).split('\n');
-        assert.ok(isVersion(lines.slice(-2, -1)[0]));
-        done();
-      });
-    });
-
-    it('multiple versions - lts/argon,12', function (done) {
-      spawn(CLI, ['lts/argon,12', '--silent', 'npm', '--version'], { stdout: 'string' }, function (err, res) {
-        assert.ok(!err);
-        var lines = cr(res.stdout).split('\n');
-        assert.ok(isVersion(lines.slice(-2, -1)[0]));
-        done();
-      });
-    });
-
-    it('one version with options - lts/erbium', function (done) {
-      spawn(CLI, ['lts/erbium', '--silent', NODE, '--version'], { stdout: 'string' }, function (err, res) {
-        assert.ok(!err);
-        var lines = cr(res.stdout).split('\n');
-        assert.ok(lines.slice(-2, -1)[0].indexOf('v12.') === 0);
-        done();
-      });
-    });
-
-    it('one version with options - lts/argon', function (done) {
-      spawn(CLI, ['lts/argon', '--silent', NODE, '--version'], { stdout: 'string' }, function (err, res) {
-        assert.ok(!err);
-        var lines = cr(res.stdout).split('\n');
-        assert.equal(lines.slice(-2, -1)[0], 'v4.9.1');
-        done();
-      });
-    });
-
-    it('multiple versions with options - 10,12,lts/erbium', function (done) {
-      spawn(CLI, ['10,12,lts/erbium', '--silent', NODE, '--version'], { stdout: 'string' }, function (err, res) {
-        assert.ok(!err);
-        var lines = cr(res.stdout).split('\n');
-        assert.ok(lines.slice(-3, -2)[0].indexOf('v10.') === 0);
-        assert.ok(lines.slice(-2, -1)[0].indexOf('v12.') === 0);
-        done();
-      });
-    });
-
-    it('multiple versions with options - 10,12,lts/erbium (sort desc)', function (done) {
-      spawn(CLI, ['10,12,lts/erbium', '--silent', '--desc', NODE, '--version'], { stdout: 'string' }, function (err, res) {
-        assert.ok(!err);
-        var lines = cr(res.stdout).split('\n');
-        assert.ok(lines.slice(-3, -2)[0].indexOf('v12.') === 0);
-        assert.ok(lines.slice(-2, -1)[0].indexOf('v10.') === 0);
-        done();
-      });
-    });
-
-    it('using engines - 12', function (done) {
-      var cwd = path.resolve(path.join(__dirname, '..', 'data', 'engines'));
-      spawn(CLI, ['engines', '--silent', NODE, '--version'], { stdout: 'string', cwd: cwd }, function (err, res) {
-        assert.ok(!err);
-        var lines = cr(res.stdout).split('\n');
-        assert.ok(lines.slice(-2, -1)[0].indexOf('v12.') === 0);
-        done();
-      });
-    });
-
-    it('using engines - 12 (--)', function (done) {
-      var cwd = path.resolve(path.join(__dirname, '..', 'data', 'engines'));
-      spawn(CLI, ['engines', '--silent', '--', NODE, '--version'], { stdout: 'string', cwd: cwd }, function (err, res) {
-        assert.ok(!err);
-        var lines = cr(res.stdout).split('\n');
-        assert.ok(lines.slice(-2, -1)[0].indexOf('v12.') === 0);
-        done();
-      });
-    });
-  });
-
-  describe('unhappy path', function () {
-    it('missing command', function (done) {
-      spawn(CLI, [], { stdout: 'string' }, function (err, res) {
-        assert.ok(!!err);
-        done();
-      });
-    });
-
-    it('missing versions', function (done) {
-      spawn(CLI, [NODE, '--version'], { stdout: 'string' }, function (err, res) {
-        assert.ok(!!err);
-        done();
-      });
-    });
-
-    it('invalid versions', function (done) {
-      spawn(CLI, ['junk,junk', NODE, '--version'], { stdout: 'string' }, function (err, res) {
-        assert.ok(!!err);
-        done();
-      });
-    });
-
-    it('engines missing', function (done) {
-      var cwd = path.resolve(path.join(__dirname, '..', 'data', 'engines-missing'));
-      spawn(CLI, ['engines', NODE, '--version'], { stdout: 'string', cwd: cwd }, function (err, res) {
-        assert.ok(!!err);
-        done();
-      });
-    });
-
-    it('engines node missing', function (done) {
-      var cwd = path.resolve(path.join(__dirname, '..', 'data', 'engines-node-missing'));
-      spawn(CLI, ['engines', NODE, '--version'], { stdout: 'string', cwd: cwd }, function (err, res) {
-        assert.ok(!!err);
-        done();
-      });
+  it('one version - 12', function (done) {
+    spawn(CLI, ['12', '--silent', 'npm', '--version'], { stdout: 'string' }, function (err, res) {
+      assert.ok(!err);
+      var lines = cr(res.stdout).split('\n');
+      assert.ok(isVersion(lines.slice(-2, -1)[0]));
+      done();
     });
   });
 });
--- test/spec/versions.test.js
+++ test/spec/versions.test.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var path = require('path');
+var rimraf = require('rimraf');
 var cr = require('cr');
 
 var nvs = require('../..');
@@ -7,130 +8,27 @@ var nvs = require('../..');
 var NODE = process.platform === 'win32' ? 'node.exe' : 'node';
 var now = new Date(Date.parse('2020-05-10T03:23:29.347Z'));
 
+var TMP_DIR = path.resolve(path.join(__dirname, '..', '..', '.tmp'));
+var OPTIONS = {
+  cacheDirectory: path.join(TMP_DIR, 'cache'),
+  installedDirectory: path.join(TMP_DIR, 'installed'),
+  buildDirectory: path.join(TMP_DIR, 'build'),
+  now: now,
+  stdout: 'string',
+  silent: true,
+};
+
 describe('versions', function () {
-  describe('happy path', function () {
-    it('one version - 12', function (done) {
-      nvs('12', NODE, ['--version'], { stdout: 'string', silent: true }, function (err, results) {
-        assert.ok(!err);
-        assert.ok(results.length > 0);
-        assert.ok(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].indexOf('v12.') === 0);
-        done();
-      });
-    });
-
-    it('lts version - lts/erbium', function (done) {
-      nvs('lts/erbium', NODE, ['--version'], { stdout: 'string', silent: true }, function (err, results) {
-        assert.ok(!err);
-        assert.ok(results.length > 0);
-        assert.ok(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].indexOf('v12.') === 0);
-        done();
-      });
-    });
-
-    it('lts/argon version - lts/argon', function (done) {
-      nvs('lts/argon', NODE, ['--version'], { stdout: 'string', silent: true }, function (err, results) {
-        assert.ok(!err);
-        assert.ok(results.length > 0);
-        assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0], 'v4.9.1');
-        done();
-      });
-    });
-
-    it('multiple versions - 10,12,lts/erbium', function (done) {
-      nvs('10,12,lts/erbium', NODE, ['--version'], { stdout: 'string', silent: true }, function (err, results) {
-        assert.ok(!err);
-        assert.ok(results.length > 0);
-        assert.ok(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].indexOf('v10.') === 0);
-        assert.ok(cr(results[1].result.stdout).split('\n').slice(-2, -1)[0].indexOf('v12.') === 0);
-        done();
-      });
-    });
-
-    it('multiple versions - 10,12,lts/erbium (sort -1)', function (done) {
-      nvs('10,12,lts/erbium', NODE, ['--version'], { sort: -1, stdout: 'string', silent: true }, function (err, results) {
-        assert.ok(!err);
-        assert.ok(results.length > 0);
-        assert.ok(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].indexOf('v12.') === 0);
-        assert.ok(cr(results[1].result.stdout).split('\n').slice(-2, -1)[0].indexOf('v10.') === 0);
-        done();
-      });
-    });
-
-    it('using engines - 12', function (done) {
-      var cwd = path.resolve(path.join(__dirname, '..', 'data', 'engines'));
-      nvs('engines', NODE, ['--version'], { stdout: 'string', cwd: cwd, silent: true }, function (err, results) {
-        assert.ok(!err);
-        assert.ok(results.length > 0);
-        assert.ok(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].indexOf('v12.') === 0);
-        done();
-      });
-    });
-
-    describe('promise', function () {
-      if (typeof Promise === 'undefined') return; // no promise support
-
-      it('using engines - 12 (promise)', function (done) {
-        var cwd = path.resolve(path.join(__dirname, '..', 'data', 'engines'));
-        nvs('engines', NODE, ['--version'], { stdout: 'string', cwd: cwd, silent: true })
-          .then(function (results) {
-            assert.ok(results.length > 0);
-            assert.ok(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].indexOf('v12.') === 0);
-            done();
-          })
-          .catch(done);
-      });
-    });
+  before(function (callback) {
+    rimraf(TMP_DIR, callback.bind(null, null));
   });
 
-  describe('unhappy path', function () {
-    it('invalid versions', function (done) {
-      nvs('1.d.4', NODE, ['--version'], { stdout: 'string', silent: true }, function (err) {
-        assert.ok(!!err);
-        done();
-      });
-    });
-
-    it('invalid versions', function (done) {
-      nvs('14,bob', NODE, ['--version'], { stdout: 'string', silent: true }, function (err) {
-        assert.ok(!!err);
-        done();
-      });
-    });
-
-    it('engines missing', function (done) {
-      var cwd = path.resolve(path.join(__dirname, '..', 'data', 'engines-missing'));
-      nvs(
-        'engines',
-        NODE,
-        ['--version'],
-        {
-          now: now,
-          stdout: 'string',
-          cwd: cwd,
-        },
-        function (err) {
-          assert.ok(!!err);
-          done();
-        }
-      );
-    });
-
-    it('engines node missing', function (done) {
-      var cwd = path.resolve(path.join(__dirname, '..', 'data', 'engines-node-missing'));
-      nvs(
-        NODE,
-        ['--version'],
-        {
-          engines: true,
-          now: now,
-          stdout: 'string',
-          cwd: cwd,
-        },
-        function (err) {
-          assert.ok(!!err);
-          done();
-        }
-      );
+  it('one version - 12', function (done) {
+    nvs('12', NODE, ['--version'], OPTIONS, function (err, results) {
+      assert.ok(!err);
+      assert.ok(results.length > 0);
+      assert.ok(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].indexOf('v12.') === 0);
+      done();
     });
   });
 });
