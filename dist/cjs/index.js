293dac62bd2b7c7ea49af8dc3942e12c9441e9ac
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-14 03:02:53 -0800

    Update to ts-dev-stack

---
+++ dist/cjs/index.js
@@ -0,0 +1,8 @@
+"use strict";
+module.exports = require("node-version-use");
+
+if ((typeof exports.default === 'function' || (typeof exports.default === 'object' && exports.default !== null)) && typeof exports.default.__esModule === 'undefined') {
+  Object.defineProperty(exports.default, '__esModule', { value: true });
+  for (var key in exports) exports.default[key] = exports[key];
+  module.exports = exports.default;
+}
