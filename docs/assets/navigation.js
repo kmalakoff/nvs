112d073a0de1aa1a202727e097f71616bbcfddc4
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-07 19:01:22 -0800

    Upgrade modules

--- docs/assets/navigation.js
+++ docs/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4uuVipJrShRslJKSU1LLM0pUdJRKkgsyVCyUipLLMpMTMpJLdaHSulllOTmKOkoZWfmpShZGRvVxgIAWnMbzD4AAAA="
+window.navigationData = "H4sIAAAAAAAAE4uuVipJrShRslJKSU1LLM0pUdJRKkgsyVCyUipLLMpMTMpJLdaHSulllOTmKOkoZWfmpShZGRvVxgIAWnMbzD4AAAA="
