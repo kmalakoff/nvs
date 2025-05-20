387f9f0da36e00d369799e7dd1544d67f96691df
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 20:28:44 -0700

    Upgrade to mandatory typescript extensions

--- test/spec/cli.test.ts
+++ test/spec/cli.test.ts
@@ -1,12 +1,14 @@
 // remove NODE_OPTIONS from ts-dev-stack
 delete process.env.NODE_OPTIONS;
 
-const assert = require('assert');
-const path = require('path');
-const spawn = require('cross-spawn-cb');
-const isVersion = require('is-version');
-const cr = require('cr');
+import assert from 'assert';
+import path from 'path';
+import url from 'url';
+import cr from 'cr';
+import spawn from 'cross-spawn-cb';
+import isVersion from 'is-version';
 
+const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 const CLI = path.join(__dirname, '..', '..', 'bin', 'cli.cjs');
 
 describe('cli', () => {
