e0061e2ec139a4357a99c4c504c72d5afe6d781f
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-17 23:42:27 -0700

    Upgrade modules

--- bin/cli.cjs
+++ bin/cli.cjs
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
 var spawn = require('cross-spawn-cb');
-spawn.sync('nvu', process.argv.slice(2), { stdio: 'inherit' })
+spawn.sync('nvu', process.argv.slice(2), { stdio: 'inherit' });
--- test/spec/versions.test.ts
+++ test/spec/versions.test.ts
@@ -2,13 +2,12 @@
 delete process.env.NODE_OPTIONS;
 
 import assert from 'assert';
-import path from 'path';
-import url from 'url';
 import cr from 'cr';
-import rimraf2 from 'rimraf2';
-
 // @ts-ignore
 import nvs from 'nvs';
+import path from 'path';
+import rimraf2 from 'rimraf2';
+import url from 'url';
 
 const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE);
 const NODE = isWindows ? 'node.exe' : 'node';
